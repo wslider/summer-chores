87ba6c806b7928a265db1b6700735199b06714cf
wslider <wslider2000@gmail.com>
2025-10-07 23:18:56 -0400

    completed .then() chain and added console.log message to log each step in promiseVersion.js

--- README.md
+++ README.md
@@ -7,10 +7,16 @@
 
 ## promiseVersion.js
 - promises to chain chores in sequence
-- 
+- intoduced increased sleepy chance via random number generation (instead of fallAsleep function) to each promise. 
+
 
 
 AI use: 
-
+ 
 callbackVersion.js 
  - used Grok 3 to search possible solutions for fallAsleep function to stop chore functions from execuiting. 
+ 
+promiseVersion.js
+ - Encountered issue with all completion messages not logging. Only the last message displayed. 
+ - Grok 3 reccomended adding console.log(value) to each .then() 
+
--- promiseVersion.js
+++ promiseVersion.js
@@ -1,47 +1,105 @@
 function mowYard() {
-  return new Promise ((resolve, reject) => {
-    setTimeout(()=> {
-        const randomNumber = Math.floor(Math.random()*100)
-        const sleepyChance = 0; 
-        if (randomNumber > sleepyChance) {
-            resolve("Bill finshed mowing the yard");
-        }
-        else {
-            reject ("Oh NO! Bill is getting tired. This never happens.");
-        }
-  }, 2000);
-  }
-);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const randomNumber = Math.floor(Math.random() * 100);
+            const sleepyChance = 0;
+            if (randomNumber > sleepyChance) {
+                resolve("Bill finished mowing the yard");
+            } else {
+                reject("Oh NO! Bill is getting tired. This never happens.");
+            }
+        }, 2000);
+    });
 }
 
-
-
-
-
-function weedEat(name, callback) {
-  setTimeout(() => {}, 1500);
+function weedEat() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const randomNumber = Math.floor(Math.random() * 100);
+            const sleepyChance = 20;
+            if (randomNumber > sleepyChance) {
+                resolve("Bill finished weed eating.");
+            } else {
+                reject("Bill fell asleep after mowing the lawn.");
+            }
+        }, 1500);
+    });
 }
 
-function trimHedges(name, callback) {
-  setTimeout(() => {}, 1000);
+function trimHedges() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const randomNumber = Math.floor(Math.random() * 100);
+            const sleepyChance = 25;
+            if (randomNumber > sleepyChance) {
+                resolve("Bill finished trimming the hedges.");
+            } else {
+                reject("Bill fell asleep after weed eating.");
+            }
+        }, 1000);
+    });
 }
 
-
-function collectWood(name, callback) {
-  setTimeout(() => {}, 2500);
+function collectWood() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const randomNumber = Math.floor(Math.random() * 100);
+            const sleepyChance = 30;
+            if (randomNumber > sleepyChance) {
+                resolve("Bill finished collecting wood.");
+            } else {
+                reject("Bill fell asleep after trimming the hedges.");
+            }
+        }, 2500);
+    });
 }
 
-function waterGarden(name, callback) {
-  setTimeout(() => {
-  }, 500);
+function waterGarden() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const randomNumber = Math.floor(Math.random() * 100);
+            const sleepyChance = 35;
+            if (randomNumber > sleepyChance) {
+                resolve("Bill finished watering the garden.");
+            } else {
+                reject("Bill fell asleep after collecting wood.");
+            }
+        }, 500);
+    });
 }
 
+function doSummerChores() {
+    return Promise.resolve("Bill finished his chores!");
+}
 
-// chain with .then and .catch
+mowYard()
+    .then(value => {
+        console.log(value); 
+        return weedEat();
+    })
+    .then(value => {
+        console.log(value); 
+        return trimHedges();
+    })
+    .then(value => {
+        console.log(value); 
+        return collectWood();
+    })
+    .then(value => {
+        console.log(value); 
+        return waterGarden();
+    })
+    .then(value => {
+        console.log(value); 
+        return doSummerChores();
+    })
+    .then(value => {
+        console.log(value); 
+    })
+    .catch(error => {
+        console.error(error); 
+    });
 
 
 
-   /* if (sleepstatus === "asleep") return;
-    console.log(`${name} mowed the yard.`);
-    sleepMessage = `${name} fell asleep after mowing the yard.`;
-    callback(); */
+
